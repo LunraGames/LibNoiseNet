f5f97c3540a2adf60050a2f6956adbaa4961d5f9
Brian Ostrander <Ostrander.Brian@gmail.com>
2016-10-21 02:59:25 -0400

    Corrected Spheres module.

--- Spheres.cs
+++ Spheres.cs
@@ -20,8 +20,6 @@
 // DEALINGS IN THE SOFTWARE.
 // 
 
-using System;
-
 namespace LibNoise
 {
     public class Spheres
@@ -40,12 +38,9 @@ namespace LibNoise
             y *= Frequency;
             z *= Frequency;
 
-            double distFromCenter = System.Math.Sqrt(x * x + y * y + z * z);
-            int xInt = (x > 0.0 ? (int)x : (int)x - 1);
-            double distFromSmallerSphere = distFromCenter - xInt;
-            double distFromLargerSphere = 1.0 - distFromSmallerSphere;
-            double nearestDist = Math.GetSmaller(distFromSmallerSphere, distFromLargerSphere);
-            return 1.0 - (nearestDist * 4.0); // Puts it in the -1.0 to +1.0 range.
+			double distFromCenter = System.Math.Sqrt(x * x + y * y + z * z);
+			var dist = 2 - (distFromCenter % 4);
+			return (dist < 0 ? 0 - dist : dist) - 1;
         }
     }
 }
